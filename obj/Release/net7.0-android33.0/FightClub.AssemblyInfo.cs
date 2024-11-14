d8fa0c1c56275db5d43753fbdb4c8aaffbcc95e6
Adrian Żabierek <a.zabierek@mercor.com.pl>
2024-11-14 13:29:31 +0100

    alst

--- obj/Release/net7.0-android33.0/FightClub.AssemblyInfo.cs
+++ obj/Release/net7.0-android33.0/FightClub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FightClub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0f8cef098847964c59c8f585cd8637351bf723ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+fe9edd45f7ccbbc76773229aecb6e7194eab4b48")]
 [assembly: System.Reflection.AssemblyProductAttribute("FightClub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FightClub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
